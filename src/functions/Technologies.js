9c6b68dd0c9f571436531fbd1696a243ac108f43
Gadiel Us <gadyus18@gmail.com>
2023-07-24 22:41:51 -0600

    Las imagenes no cargan, se arreglo en mi computadora pero ahora veremos en el navegador

--- src/functions/Technologies.js
+++ src/functions/Technologies.js
@@ -14,11 +14,11 @@ const Ejs = Technologies[11]
 const Express = Technologies[12]
 
 const ListProject =[
-    {name: 'Suma De Matrices', Use: [Pug, Sass, JavaScript], img: '/SumaMatrices.PNG',
+    {name: 'Suma De Matrices', Use: [Pug, Sass, JavaScript], img: './SumaMatrices.PNG',
     detail: 'Aplicación web desarrollada para la suma de Matrices', urlGithub: 'https://giuc777.github.io/AlgebraLinealProyectoMatrices/', deploy: 'https://github.com/giuc777/AlgebraLinealProyectoMatrices/settings/pages'},
-    {name: 'YardSale', Use: [Html, Css, JavaScript], img: '/Yard.png',
+    {name: 'YardSale', Use: [Html, Css, JavaScript], img: './Yard.png',
     detail: 'Pagina dinamica de compras', urlGithub: 'https://github.com/giuc777/curso_practico_js', deploy: 'https://giuc777.github.io/curso_practico_js/'},
-    {name: 'Crypfirm', Use: [Express, Ejs, Css, Bootstrap, MariaDB], img: '/crypfirm.PNG',
+    {name: 'Crypfirm', Use: [Express, Ejs, Css, Bootstrap, MariaDB], img: './crypfirm.PNG',
     detail: 'Aplicación web desarrollada para la verificación y creación de fimras electronicas.', urlGithub: 'https://github.com/giuc777/Crypfirm', deploy: 'https://github.com/giuc777/Crypfirm'}
 ]
 
